4f521a06577fae79512dddb6a5d22592102dfb2d
Loic-974 <loic.rabat@live.fr>
2020-11-14 20:24:01 +0400

    Ui Brand look likke good

--- public/js/Production/production.js
+++ public/js/Production/production.js
@@ -7,11 +7,26 @@ const LinkBrand = document.querySelector("#brandLink");
 const LinkCategorie = document.querySelector("#CategorieLink");
 const LinkProduct = document.querySelector("#ProductLink");
 
+const containerArray = document.querySelector('.table-content-left')
+const ArrayOfItems = document.querySelector("#ArrayProduction");
+
 const selectBrand = document.querySelector("#SelectBrand");
 const selectCategorie = document.querySelector("#SelectCategorie");
 const selectYear = document.querySelector("#SelectAnnee");
 const inputRange = document.querySelector("#SelectPrice");
-const ArrayOfItems = document.querySelector("#ArrayProduction");
+const selectBrandForm = document.querySelector("#SelectBrandForm");
+const selectCatForm = document.querySelector("#SelectCategorieForm");
+
+const filterGroup = document.querySelectorAll(".filter-container")[0];
+
+const formBrandName = document.querySelector("#newBrandName");
+const formCatName = document.querySelector("#newCatName");
+const formProductName = document.querySelector("#newProductName");
+const formYearInput = document.querySelector('#newYearProduct');
+const formPriceInput = document.querySelector('#newPriceProduct');
+const formGroupSelect = document.querySelector('.group-selectProduct')
+
+
 
 // -----------------------------------------------------------------------
 //--------------------------------- Get Data ------------------------------
@@ -30,7 +45,8 @@ fetch("/production/getBrand", {
         for (let brand of response) {
             brands.push(brand);
         }
-    });
+    })
+    .then(() => LinkBrand.click());
 
 fetch("/production/getCat", {
     method: "GET",
@@ -65,73 +81,125 @@ fetch("/production/getProduct", {
 // ----------------------------------------------------------------------------
 // --------------------------------- Build View -------------------------------
 // ----------------------------------------------------------------------------
-window.onload = () => {
-    insertSelectBrand(), buildArray(brands);
-    insertSelectCategorie(), insertSelectYears();
-};
+
+    containerArray.style.display='none';
+    formBrandName.style.display = "inline";
+    formCatName.style.display = "none";
+    formProductName.style.display = "none";
+    formYearInput.style.display ='none'
+    formPriceInput.style.display='none'
+    formGroupSelect.style.display='none'
+    disabledSelectFilter([selectCategorie,selectYear,selectBrand,inputRange])
+
+
 
 LinkBrand.onclick = () => {
     buildArray(brands);
+    formBrandName.style.display = "inline-block";
+    formCatName.style.display = "none";
+    formProductName.style.display = "none";
+    formYearInput.style.display ='none'
+    formPriceInput.style.display='none'
+    formGroupSelect.style.display='none'
+    disabledSelectFilter([selectCategorie,selectYear,selectBrand,inputRange])
 };
 
 LinkCategorie.onclick = () => {
     buildArray(categorie);
+    formBrandName.style.display = "none";
+    formCatName.style.display = "inline-block";
+    formProductName.style.display = "none";
+    formYearInput.style.display ='none'
+    formPriceInput.style.display='none'
+    formGroupSelect.style.display='none'
+    disabledSelectFilter([selectCategorie,selectYear,selectBrand,inputRange])
 };
 
 LinkProduct.onclick = () => {
-    insertSelectBrand(), buildArray(produits);
-    insertSelectCategorie(), insertSelectYears();
+    insertSelectBrand([selectBrand, selectBrandForm]), buildArray(produits);
+    insertSelectCategorie([selectCategorie, selectCatForm]),
+        insertSelectYears();
+        formBrandName.style.display = "none";
+        formCatName.style.display = "none";
+        formProductName.style.display = "inline-block";
+        formYearInput.style.display ='inline-block'
+        formPriceInput.style.display='inline-block'
+        formGroupSelect.style.display='block'
+        selectBrand.disabled=false
+        selectCategorie.disabled=false
+        allowSelectFilter([selectCategorie,selectYear,selectBrand,inputRange])
 };
 
-// selectItem.onchange= () => {
-//     console.log(selectItem.value)
-//     buildArray(filterArray(brands,selectItem.value))
-// }
+// ---------------------------------------------------------------------------- //
+// ---------------------------- Add Data to Select Or input ------------------- //
+// ---------------------------------------------------------------------------- //
 
-function insertSelectBrand() {
-    selectBrand.innerHTML = "";
-    for (let ref of brands) {
-        selectBrand.insertAdjacentHTML(
-            "beforeend",
-            '<option value="' + ref.brandId + '">' + ref.brandName + "</option>"
-        );
+function disabledSelectFilter(filter){
+    
+    for(let filtre of filter){
+        console.log(filtre)
+        filtre.disabled=true
     }
 }
 
-function insertSelectCategorie() {
-    selectCategorie.innerHTML = "";
-    for (let ref of categorie) {
-        selectCategorie.insertAdjacentHTML(
-            "beforeend",
-            '<option value="' +
-                ref.categoryId +
-                '">' +
-                ref.categoryName +
-                "</option>"
-        );
+function allowSelectFilter(filter){
+    for(let filtre of filter){
+        filtre.disabled=false
     }
-    console.log("------- Loaded Select --------");
 }
 
+function insertSelectBrand(select) {
+    for (let input of select) {
+        input.innerHTML = "";
+        for (let ref of brands) {
+            input.insertAdjacentHTML(
+                "beforeend",
+                '<option value="' +
+                    ref.brandId +
+                    '">' +
+                    ref.brandName +
+                    "</option>"
+            );
+        }
+    }
+}
 
+function insertSelectCategorie(select) {
+    for (let input of select) {
+        input.innerHTML = "";
+        for (let ref of categorie) {
+            input.insertAdjacentHTML(
+                "beforeend",
+                '<option value="' +
+                    ref.categoryId +
+                    '">' +
+                    ref.categoryName +
+                    "</option>"
+            );
+        }
+        console.log("------- Loaded Select --------");
+    }
+}
 
-function setYearPresentInProduct(){
-
+function setYearPresentInProduct() {
     let arrayOfYear = [];
     // recupere distinctement les dates en fonction des produits
     for (let ref of produits) {
-            if(!arrayOfYear.includes(ref.model_year)){
-                arrayOfYear.push(ref.model_year)
-            }
+        if (!arrayOfYear.includes(ref.model_year)) {
+            arrayOfYear.push(ref.model_year);
+        }
     }
-    return arrayOfYear
+    return arrayOfYear;
 }
 
 function insertSelectYears() {
     selectYear.innerHTML = "";
-    array =  setYearPresentInProduct()
-    for(let annee of array){
-    selectYear.insertAdjacentHTML('beforeend','<option value="'+ annee+'">' +annee + '</option>')
+    array = setYearPresentInProduct();
+    for (let annee of array) {
+        selectYear.insertAdjacentHTML(
+            "beforeend",
+            '<option value="' + annee + '">' + annee + "</option>"
+        );
     }
 }
 
@@ -140,6 +208,8 @@ function insertSelectYears() {
 //--------------------------------------------------------------//
 
 function buildArray(array) {
+    containerArray.style.display='block'
+    const dontShowString = "_id";
     ArrayOfItems.innerHTML = "";
     let thead = document.createElement("thead");
     let tbody = document.createElement("tbody");
@@ -148,15 +218,20 @@ function buildArray(array) {
         let tr = document.createElement("tr");
         for (let propriete in ref) {
             if (i === 0) {
-                let th = document.createElement("th");
-                let thText = document.createTextNode(propriete);
-                th.appendChild(thText);
-                thead.appendChild(th);
+                // limit 1 header
+                if (!propriete.includes(dontShowString)) {
+                    let th = document.createElement("th");
+                    let thText = document.createTextNode(propriete);
+                    th.appendChild(thText);
+                    thead.appendChild(th);
+                }
             }
-            let td = document.createElement("td");
-            let tdText = document.createTextNode(ref[propriete]);
-            td.appendChild(tdText);
-            tr.appendChild(td);
+            if (!propriete.includes(dontShowString)) {
+                let td = document.createElement("td");
+                let tdText = document.createTextNode(ref[propriete]);
+                td.appendChild(tdText);
+                tr.appendChild(td);
+            }
         }
         tbody.appendChild(tr);
         i++;
@@ -179,6 +254,10 @@ function filterArray(array, value) {
     console.log(result);
     return result;
 }
+
+// ----------------------------------------------------------------------- //
+// ---------------------------- Display ---------------------------------- //
+// ----------------------------------------------------------------------- //
 
 console.log(brands);
 console.log(categorie);
--- resources/views/production.blade.php
+++ resources/views/production.blade.php
@@ -33,15 +33,40 @@
         </div>
         <div class='content-right'>
 
-            <h4>Filtrer le Tableau<h4>
-            <select id='SelectBrand'></select>
-            <select id='SelectCategorie'></select>
-            <select id='SelectAnnee'><select>
-        
-            <input type='range' id="SelectPrice" value="2500" step="10" min="0" max="12000" oninput="valueRange.value = SelectPrice.value +'€'">
-            <output id="valueRange">2500€</output>
-            <h4> Modificatio et Ajout <h4>
-            <input type='texte' value='' placeholder='Entrer une nouvelle donnée'>
+                <h4>Filtres</h4>
+                <div class='filter-container'>
+                <select id='SelectBrand'></select>
+                <select id='SelectCategorie'></select>
+                <select id='SelectAnnee'><select>
+            
+                <input type='range' class='form-control-range' id="SelectPrice" value="2500" step="10" min="0" max="12000" oninput="valueRange.value = SelectPrice.value +'€'">
+                <output id="valueRange">2500€</output>
+            </div>
+            <div class='form-container'>
+              <h4> Ajouts </h4>
+                <form>
+                    <div class='form-row'>
+                            <input type='texte' id='newBrandName' class='form-control' value='' placeholder='Nouvelle Marque'>
+                    </div>
+                    <div class='form-row'>
+                            <input type='texte' id='newCatName' class='form-control' value='' placeholder='Nouvelle Catégorie'>              
+                    </div>
+                    <div class='form-row'>
+                            <input type='texte' id='newProductName' class='form-control' value='' placeholder='Nouveau Produit'>
+                    </div>
+                    <div class='form-row'>
+                            <input type='number' id='newYearProduct' class='form-control' value='' placeholder='Année du Modèle'>
+                    </div>
+                    <div class='form-row'>
+                            <input type='number' id='newPriceProduct' class='form-control' value='' placeholder='Prix du Modèle'>
+                    </div>
+                    <div class='form-row group-selectProduct'>
+                    <select id='SelectBrandForm'></select>
+                    <select id='SelectCategorieForm'></select>
+                    </div>
+                    <input type='button' value='Confirmer' class='btn-primary' id='btnAddDataBrand'>
+                </form>
+            </div>
         </div>
      
         </div>
