6a03fcfcc176c6f606f405e74fb5446b6879f5fb
Loic-974 <loic.rabat@live.fr>
2020-11-14 15:14:48 +0400

    Ajout de Boostrap

--- resources/views/production.blade.php
+++ resources/views/production.blade.php
@@ -5,7 +5,8 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
         <link href="{{ asset('css/home.css') }}" rel="stylesheet">
-       
+        <link href="{{ asset('css/section.css') }}" rel="stylesheet">
+        <link href="{{asset('css/bootstrap.min.css')}}" rel='stylesheet'>
     </head>
 
   
@@ -15,51 +16,35 @@
 
      <div class='container-content'>
         <div class='sub-container'>
-            <nav id="">
-            <ul class="nav">
-                <li class="nav-item nav-link active" id="brandLink">
-                    Brand
-                </li>
-                <li class="nav-item" id="CategorieLink">
-                    Categorie
-                </li>
-                <li class="nav-item" id="ProductLink">
-                    Product
-                </li>
-                <li class="nav-item">
-                    Stock
-                </li>
-            </ul>
-            </nav>
-
+        <div class='nav'>
+            <div class="btn-group" role="group">
+                <button type="button" class="btn btn-secondary" id="brandLink">Marques</button>
+                <button type="button" class="btn btn-secondary"  id="CategorieLink">Categories</button>
+                <button type="button" class="btn btn-secondary" id="ProductLink">Produits</button>
+                <button type="button" class="btn btn-secondary" id="StockLink">Stock</button>
+            </div>
+        </div>
+        <div class='view-content'>
         <div class='content-left'>
-
-            <div id="brandView">
                 <div class='table-content-left'>
-                    <table  id="ArrayProduction">
+                    <table  class ='table table-dark' id="ArrayProduction">
                     </table>
                 </div>
-            </div>
-
-
         </div>
         <div class='content-right'>
 
-        <select id='SelectBrand'></select>
-        <select id='SelectCategorie'></select>
-        <select id='SelectAnnee'><select>
-      
-        <input type='range' id="SelectPrice" value="2500" step="10" min="0" max="12000" oninput="valueRange.value = SelectPrice.value +'€'">
-        <output id="valueRange">2500€</output>
-
-        <input type='texte' value='' placeholder='Entrer une nouvelle donnée'>
-
-
-
-
+            <h4>Filtrer le Tableau<h4>
+            <select id='SelectBrand'></select>
+            <select id='SelectCategorie'></select>
+            <select id='SelectAnnee'><select>
+        
+            <input type='range' id="SelectPrice" value="2500" step="10" min="0" max="12000" oninput="valueRange.value = SelectPrice.value +'€'">
+            <output id="valueRange">2500€</output>
+            <h4> Modificatio et Ajout <h4>
+            <input type='texte' value='' placeholder='Entrer une nouvelle donnée'>
         </div>
      
-     
+        </div>
      
      
      
